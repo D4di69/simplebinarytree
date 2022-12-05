e97e5481a71cdc3f1a20d61b20f31f0bd43972a6
D4di69 <99908436+D4di69@users.noreply.github.com>
2022-12-05 09:08:12 +0100

    second_version
    added calculation of length and depth

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 node_parent = {0: None}
 node_data = {0: "root"}
 node_position = {0: None}
+tree_length =  len((node_data))
+tree_depth = 0
+unique_parents = []
 # 0 is the identifier number of the root node
+
+def getDepth():
+    listOfValues = list(node_parent.values())
+    for value in listOfValues:
+        if value not in unique_parents:
+            unique_parents.append(value)
+    tree_depth = len(unique_parents)
 
 def getKeysByValue(dictOfElements, valueToFind): # This should be ignored, it is just a function that creates a list of all key names that contain a searched value in a dict (for parent max)
     listOfKeys = list()
@@ -21,5 +31,7 @@ def createNode(position, parent, value): # For position: 0 = Root 1 = Left and 2
         node_parent[temp_identifier] = parent
         node_data[temp_identifier] = value
         node_position[temp_identifier] = position
+        tree_length =  len((node_data))
+        getDepth()
 
 
